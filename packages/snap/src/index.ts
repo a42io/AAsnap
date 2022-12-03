7f27462bdb684a6bce1cf9eb982d5274d0ac31cf
Hideyoshi Moriya <hide@piyo.jp>
2022-12-03 13:30:41 +0530

    remove comments

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,25 +1,8 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
 
-/**
- * Get a message from the origin. For demonstration purposes only.
- *
- * @param originString - The origin string.
- * @returns A message based on the origin.
- */
 export const getMessage = (originString: string): string =>
   `Hello, ${originString}!`;
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns `null` if the request succeeded.
- * @throws If the request method is not valid for this snap.
- * @throws If the `snap_confirm` call failed.
- */
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
